47f9f92b82633720cbeb3f8c7cd375b71c8e685c
Tech Guy <43686905+TechScripts@users.noreply.github.com>
2020-10-27 19:07:56 +0530

    Create Add (Append) text to file.ps1

---
+++ Add (Append) text to file.ps1
@@ -0,0 +1,15 @@
+# Change directory to the required folder 
+# Modify values as needed.
+
+foreach ($filepath in (Get-ChildItem -Recurse -Include *index.md).fullname) {
+
+  $fileContent = Get-Content $filePath
+  #$fileContent[$lineNumber+1] += [Environment]::NewLine
+  $fileContent[$lineNumber+2] +=  [Environment]::NewLine + 'tags: ["Tech Articles"]'
+  $fileContent[$lineNumber+2] += [Environment]::NewLine + 'categories: ["Tech Articles"]'
+  $fileContent | Set-Content $filePath 
+
+}
+
+
+
