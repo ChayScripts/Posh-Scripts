a984ef16f3f4254c0dfd7fc702027921b9fcf138
Tech Guy <43686905+TechScripts@users.noreply.github.com>
2020-10-27 19:08:27 +0530

    Update Add (Append) text to file.ps1

--- Add (Append) text to file.ps1
+++ Add (Append) text to file.ps1
@@ -4,7 +4,6 @@
 foreach ($filepath in (Get-ChildItem -Recurse -Include *index.md).fullname) {
 
   $fileContent = Get-Content $filePath
-  #$fileContent[$lineNumber+1] += [Environment]::NewLine
   $fileContent[$lineNumber+2] +=  [Environment]::NewLine + 'tags: ["Tech Articles"]'
   $fileContent[$lineNumber+2] += [Environment]::NewLine + 'categories: ["Tech Articles"]'
   $fileContent | Set-Content $filePath 
